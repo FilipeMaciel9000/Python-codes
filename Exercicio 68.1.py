bd3ce991fb45c672f3272ee3feac6b12371d59ec
Filipe Maciel <83095857+FilipeMaciel9000@users.noreply.github.com>
2022-06-30 16:59:03 -0300

    Update Exercicio 68.1.py

--- Exercicio 68.1.py
+++ Exercicio 68.1.py
@@ -14,11 +14,18 @@
     c)mão fechada = pedra
         *Pedra quebra tesoura = vitoria de quem escolheu pedra.
 3) Quem ganha: São três rodadas, quem tiver mais vitorias ganha;
-(1) Bibliotecas usadas:
-(2)
-(3)
-(4)
-(5)
+(1) Bibliotecas usadas: RANDOM para tornar aleatoria a escolha do computador.
+                        TIME para fazer o programa falar pedra, papel, tesoura.
+(2) Variaveis de contagem começam com zero;
+(3) Variável jogador começa com uma string vazia;
+(4) Variável continuar começa com S(sim) porque se o usuário obri o progrma subentende-se que ele quer começar a jogar;
+(5) Opções de jogadas possiveis para o computador;
+(6) Enfeite pra deichar o codigo bonito no final;
+(7) Estrutura de repetição While que faz o codigo rodar enquanto o usuario digitar N para parar;
+(8) Estrutura de repetição For que faz as três jogadas do jogo;
+(9) Estruturas condicionais que definem o destino do jogo;
+(10) Se continuar (7) for diferente de S o programa encerra mostrando o  resultado do jogo;
+(11) Mostra o resultado do jogo;
 """
 # (1)
 from random import choice
@@ -28,13 +35,13 @@ vitoria = 0  # (2)
 derrota = 0
 empate = 0
 jogador = ' '  # (3)
-continuar = 'S'
-opcoes = ['pedra', 'papel', 'tesoura']  # (4)
-print('=' * 50)
-while continuar == 'S':
-    for v in range(1, 4):  # (5)
-        print('Vamos jogar pedra, papel, tesoura!')
-        jogador = str(input('Digite pedra, papel ou tesoura para jogar: '))
+continuar = 'S' # (4)
+opcoes = ['pedra', 'papel', 'tesoura']  # (5)
+print('=' * 50) # (6)
+while continuar == 'S': # (7)
+    for v in range(1, 4):  # (8)
+        print('Vamos jogar pedra, papel, tesoura!') 
+        jogador = str(input('Digite pedra, papel ou tesoura para jogar: ')) 
         computador = choice(opcoes)
         sleep(1)
         print('Pedra')
@@ -42,7 +49,7 @@ while continuar == 'S':
         print('Papel')
         sleep(1)
         print('Tesoura')
-        if jogador == "pedra" and computador == 'pedra':
+        if jogador == "pedra" and computador == 'pedra': # (9)
             print('Empate')
             empate += 1
         elif jogador == "pedra" and computador == 'tesoura':
@@ -72,10 +79,10 @@ while continuar == 'S':
         print('Você jogou {} o computador jogou {}!'.format(jogador, computador))
     continuar = str(input('Deseja continuar[S/N]: ')).strip().upper()[0]
     print('=' * 50)
-    if continuar == 'N':
+    if continuar == 'N': #(10)
         break
 print('Fim de jogo!')
-print('Você ganhou {} vezes; \nEmpatou {} vezes! \nE o computador ganhou {} vezes!'.format(vitoria, empate, derrota))
+print('Você ganhou {} vezes; \nEmpatou {} vezes! \nE o computador ganhou {} vezes!'.format(vitoria, empate, derrota)) #(11)
 if vitoria > derrota:
     print('*:･ﾟ✧ ✧ﾟ･: * VITÓRIA DO JOGADOR !!! *:･ﾟ✧ ✧ﾟ･: *')
 else:

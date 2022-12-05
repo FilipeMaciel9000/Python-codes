1c74efd535dcd9c28764e39f650b18600c97c0d3
Filipe Maciel <83095857+FilipeMaciel9000@users.noreply.github.com>
2022-12-05 09:46:24 -0300

    Update Caminho.py

--- Caminho.py
+++ Caminho.py
@@ -3,6 +3,6 @@ print(' # # # # # EL CAMINO # # # # # ')
 print(' # '*10)
 i = int(input("Digite o numéro de inicio: ")) # NUMÉRO ONDE A CONTAGEM COMEÇARA
 f = int(input("Digite o numéro do fim: ")) # NUMÉRO ONDE A CONTAGEM TERMINARA
-p = int(input("Digite o numéro de saltos: ")) # NUMÉRO PELO QUAL A CONTAGEM SERÁ FEITA
-for c in range (i, f+1, p):
-    print(c)
+s = int(input("Digite o numéro de saltos: ")) # NUMÉRO PELO QUAL A CONTAGEM SERÁ FEITA
+for i in range (i, f+1, s):
+    print(i)

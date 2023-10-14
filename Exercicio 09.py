b979072ffbe4708826160c79454ec6d75e27d2e3
Filipe Maciel <83095857+FilipeMaciel9000@users.noreply.github.com>
2023-10-14 15:53:07 -0300

    Exercicio 09.py

--- Exercicio 09.py
+++ Exercicio 09.py
@@ -1,28 +1,32 @@
 '''
 Achei melhor fazer logo com a estrutura while
 '''
-n = int(input(' Digite o numero da tabuada que você quer estudar: \n '))
-x = 1
-y = 1
-z = 1
-w = 1
-print('DIVISÃO°{}\n'.format(n))
-while x <= 10:
-    d = x / n
-    print('{} / {} = {:.2f}'.format(x, n, d))
-    x += 1
-print('Multiplicação°{}\n'.format(n))
-while y <= 10:
-    m = n * y
-    print('{} * {} = {}'.format(n, y, m))
-    y += 1
-print('SUBTRAÇÃO°{}\n'.format(n))
-while z <= 10:
-    s = z - n
-    print('{} - {} = {}'.format(z, n, s))
-    z += 1
-print('ADIÇÃO°{}\n'.format(n))
-while w <= 10:
-    a = n + w
-    print('{} + {} = {}'.format(n, w, a))
-    w += 1
+n = int(input ('insera o número da tabuada que você quer estudar: '))
+c = 1
+i = 1
+a = 1
+b = 1
+
+print ('Tabuada de adição de {}: \n'.format(n))
+while a<= 10:
+  som = n+a
+  print ('{} + {} = {} \n'.format(n, a, som))
+  a+= 1
+
+print('Tabuada de multiplicação de {}: \n'.format(n))
+while b<= 10:
+  m = n*b
+  print ('{} X {} = {} \n'.format(n, b, m))
+  b+= 1
+  
+print ('Tabuada de divisão de {}: \n'.format(n))
+while i<= 10:
+  d = (i*n)/n
+  print ('{} / {} = {:.0f} \n'.format(i*n, n, d))
+  i += 1
+  
+print ('Tabuada de subitração de {}: \n'.format(n))
+while c<= 10:
+  sub = (c+n)-n
+  print ('{} - {} = {} \n'.format(c+n, n, sub))
+  c += 1
